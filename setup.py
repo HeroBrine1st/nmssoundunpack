54432aec1eb2b8ec9efe41e8cb5cf6ca4e25b3f8
HeroBrine1st Erquilenne <herobrine1.videos@gmail.com>
2022-08-01 21:36:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+import os
 from distutils.core import setup
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+
+# https://stackoverflow.com/a/53069528
+requirement_path = lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name="nmssoundunpack",
@@ -8,7 +17,7 @@ setup(
     author="HeroBrine1st Erquilenne",
     url="https://github.com/HeroBrine1st/nmssoundunpack",
     packages=["nmssoundunpack"],
-    requires=["rich", "xmltodict"],
+    install_requires=install_requires,
     package_data={
         "nmssoundunpack": [
             "tools/ww2ogg/COPYING",
