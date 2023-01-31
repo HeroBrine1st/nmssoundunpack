e4f10aed69cefcdbe9bcdc3cbfa67c5aa09ef858
herobrine1st <pankov.76.tvink@gmail.com>
2023-01-31 15:14:07 +0300

    Handle legacy versions

--- nmssoundunpack/main.py
+++ nmssoundunpack/main.py
@@ -3,6 +3,7 @@ import hashlib
 import os.path
 import shutil
 import signal
+import sys
 from pathlib import Path
 from typing import Dict, Tuple
 
@@ -15,15 +16,18 @@ from rich.text import Text
 from nmssoundunpack.lib import PSArc, get_psarc_paths, count_files_in_psarc, unpack_psarc, get_wem_file_path, \
     process_wem_file
 
-psarcs: list[PSArc] = [
+psarcs_latest: list[PSArc] = [
     PSArc("NMSARC.5B11B94C.pak", "AUDIO/WINDOWS"),
 ]
 
-psarcs_old: list[PSArc] = [
+psarcs_next: list[PSArc] = [
     PSArc("NMSARC.5B11B94C.pak", "AUDIO/WINDOWS"),
     PSArc("NMSARC.FE28D146.pak", "AUDIO")
 ]
 
+psarcs_legacy: list[PSArc] = [
+    PSArc("NMSARC.B9700502.pak", "AUDIO")
+]
 
 run = True
 
@@ -45,12 +49,20 @@ def main():
     args = parser.parse_args()
     source = Path.cwd() / Path(args.source)
     destination = Path.cwd() / Path(args.destination)
-    tmp = Path.cwd() / Path(args.tmp) if args.tmp is not None else destination / Path(
-        __file__).parent.name
+    tmp = Path.cwd() / Path(args.tmp) if args.tmp is not None else destination / Path(__file__).parent.name
     keep = args.keep
-    old = args.old
+    compat = args.compatibility
 
-    for psarc in (psarcs_old if old else psarcs):
+    if compat is None:
+        psarcs = psarcs_latest
+    elif compat == "next":
+        psarcs = psarcs_next
+    elif compat == "legacy":
+        psarcs = psarcs_legacy
+    else:
+        raise RuntimeError
+
+    for psarc in psarcs:
         psarc_path = source / psarc.name
         if not psarc_path.exists():
             print(f"File {psarc_path} is not found in {source}")
@@ -59,16 +71,26 @@ def main():
 
     console = Console()
 
-    if not all(get_psarc_paths(psarc, source, tmp)[1].is_dir() for psarc in psarcs):
-        with Progress(  # It leaves empty line if no tasks were created
+    # Skip already unpacked
+    if not all(is_complete(get_psarc_paths(psarc, source, tmp)[1]) for psarc in psarcs):
+        with Progress(  # It leaves empty line when no task is created
                 *get_progress_bar_columns(True),
                 console=console,
                 expand=True) as progress:
+            tip_printed = False
             for psarc in psarcs:
                 psarc_path, unpack_dir = get_psarc_paths(psarc, source, tmp)
                 if unpack_dir.is_dir():
-                    console.print(f"Skipping \"{psarc.name}\"")
-                    continue
+                    if (unpack_dir / "COMPLETE").is_file():
+                        console.print(f"Skipping \"{psarc.name}\"")
+                        continue
+                    else:
+                        shutil.rmtree(unpack_dir)
+                        console.print(f"Removed uncompleted extraction of \"{psarc.name}\".")
+                        if not tip_printed:
+                            console.print("Tip: do not interrupt program while it is extracting as there's no way "
+                                          "to gracefully shutdown extraction while keeping performance good")
+                            tip_printed = True
                 task = progress.add_task(f"Extracting \"{psarc.name}\"", start=False)
                 files_count = count_files_in_psarc(psarc_path)
                 progress.update(task, total=files_count)
@@ -77,6 +99,7 @@ def main():
                     Text("Extracting ").append(stylize_path(psarc_path)).append(" to ").append(stylize_path(unpack_dir))
                 )
                 unpack_psarc(psarc_path, unpack_dir, lambda x: progress.update(task, advance=1))
+                (unpack_dir / "COMPLETE").touch()
     else:
         for psarc in psarcs:
             console.print(f"Skipping \"{psarc.name}\"")
@@ -106,8 +129,8 @@ def main():
                             if dict_file_md5.digest() != this_file_md5.digest():  # Just in case
                                 console.print(
                                     Text("File ").append(stylize_path(converted_path)).append(" has collision:"))
-                                console.print(stylize_path(files[converted_path]))
-                                console.print(stylize_path(wem_path))
+                                console.print(Text(" - ").append(stylize_path(files[converted_path])))
+                                console.print(Text(" - ").append(stylize_path(wem_path)))
                                 counter = 1
                                 split_path = list(os.path.splitext(os.fspath(converted_path)))
                                 split_path.insert(-1, f"_{counter}")
@@ -131,7 +154,7 @@ def main():
     # Add handlers so that Ctrl+C doesn't break files
     signal.signal(signal.SIGINT, interrupt)
     signal.signal(signal.SIGTERM, interrupt)
-    signal.signal(signal.SIGHUP, interrupt)
+    if sys.platform == "linux": signal.signal(signal.SIGHUP, interrupt),
     console.print(f"{len(files)} files to proceed")
     converted = 0
     errors = []
@@ -141,20 +164,27 @@ def main():
             *get_progress_bar_columns(True),
             console=console, expand=True) as progress:
         task = progress.add_task("Converting files", total=len(files))
-        for destination in files:
-            source = files[destination]
+        for destination, source in files.items():
             if not destination.exists():
+                tmp_dest = destination.with_suffix(".tmp")
+                if tmp_dest.is_file():
+                    tmp_dest.unlink()
+                    console.print(Text("Cleared ").append(stylize_path(tmp_dest)))
                 try:
-                    process_wem_file(source, destination)
-                    converted += 1
+                    process_wem_file(source, tmp_dest)
                 except Exception:
+                    tmp_dest.unlink(missing_ok=True)
                     errors.append((source, destination))
                     console.print(f"An error occurred while converting {source} to {destination}")
                     console.print_exception()
+                else:
+                    tmp_dest.rename(destination)
+                    converted += 1
             else:
                 skipped += 1
             progress.update(task, advance=1, description=f"Converting files ({skipped} skipped, {len(errors)} errors)")
             if not run:
+                console.print("Gracefully shutting down...")
                 break
     if run and not keep:
         console.print("Clearing temporary directory")
@@ -172,7 +202,12 @@ def main():
 # noinspection PyUnusedLocal
 def interrupt(signal_num, stack_frame):
     global run
-    run = False
+    if run:
+        print("Interrupt signal received.")
+        run = False
+    else:
+        print("Emergency shutdown - got second interrupt")
+        sys.exit(127)
 
 def md5_of_file(file: Path, progress: Progress):
     md5 = hashlib.md5()
@@ -203,16 +238,21 @@ def stylize_path(path: Path) -> Text:
     filename.stylize("repr.filename")
     return parent.append(filename)
 
+def is_complete(unpack_dir: Path) -> bool:
+    return unpack_dir.is_dir() and (unpack_dir / "COMPLETE").is_file()
+
+
 parser = argparse.ArgumentParser(description="Unpack, convert and catalog No Man's Sky sound assets")
-parser.add_argument("--source", type=dir_path, default=os.getcwd(), metavar="path",
+parser.add_argument("-s", "--from", "--source", dest="source", type=dir_path, default=os.getcwd(), metavar="path",
                     help="Source directory (usually game installation directory plus GAMEDATA/PCBANKS)")
-parser.add_argument("--destination", type=new_dir_path, default=os.getcwd(), metavar="path",
+parser.add_argument("-d", "--to", "--destination", dest="destination", type=new_dir_path, default=os.getcwd(),
+                    metavar="path",
                     help="Destination directory")
-parser.add_argument("--tmp", type=new_dir_path, metavar="path",
+parser.add_argument("--tmp", type=new_dir_path, metavar="path", dest="tmp",
                     help="Temporary directory to unpack archives")
-parser.add_argument("-k", "--keep", action="store_const", const=True, default=False, dest="keep",
+parser.add_argument("-k", "--keep", action="store_true", dest="keep",
                     help="Keep temporary files")
-parser.add_argument("--old", action="store_const", const=True, default=False, dest="old",
+parser.add_argument("-c", "--compat", "--compatibility", dest="compatibility", choices=["legacy", "next"],
                     help="Enable compatibility for older versions")
 
 if __name__ == "__main__":
